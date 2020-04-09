1321af9ed957ce8ae6bfe2591b28d084c1f4e6ce
Gallen Calade <gallencalade@qq.com>
2020-04-09 17:53:45 +0800

    save

--- clang/memlayout2/example/src/main.cc
+++
@@ -1,7 +0,0 @@
-#include "demo.h"
-
-int main(int argc, char* argv[]) {
-  demo::Demo d;
-  d.run();
-  return 0;
-}
--- clang/memlayout2/run_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j4
-
-./memlayout -p='../example/build' .
